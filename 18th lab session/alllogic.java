b58d7b485b1a1e28b02aae185afe48df625c8729
Subrata1906 <110475131+Subrata1906@users.noreply.github.com>
2022-08-21 18:14:15 +0530

    Add files via upload

---
+++ 18th lab session/alllogic.java
@@ -0,0 +1,22 @@
+package javaproject;
+
+public class alllogic {
+
+	public static void main(String[] args) {
+		int m = (200 + 100) / 3;
+	double n = 3.0-6* 10000000.1 ;
+	boolean o =  true && true;
+	boolean p =  false && true;
+	boolean q =  (false && false) || (true && true);
+	boolean r = (false || false) && (true && true);
+
+	System.out.println("(200 + 100) / 3->"+m);
+	System.out.println("3.0-6* 10000000.1->"+n);
+	System.out.println("true && true->"+o);
+	System.out.println("false && true->"+p);
+	System.out.println("(false && false) || (true && true)->"+q);
+	System.out.println("(false || false) && (true && true->"+r);
+	
+	}
+
+}
