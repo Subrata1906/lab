557bad4a261f20bf3ea53cc5d72c38263ded99a6
Subrata1906 <110475131+Subrata1906@users.noreply.github.com>
2022-10-12 17:23:44 +0530

    Add files via upload

---
+++ 10th_oct/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+      int[] myArray = {1789,2035,1899,1456,2013};
+      int num = 2013;
+
+      for(int i = 0; i<myArray.length; i++){
+         if(num == myArray[i]){
+            System.out.println("True");
+         }
+         else{
+         Syatem.out.println("False");
+         }}}}
---
+++ 10th_oct/OddPosition.java
@@ -0,0 +1,14 @@
+
+		public class OddPosition {  
+    public static void main(String[] args) {  
+        //Initialize array  
+       int [] arr = new int [] {a,b,c,d,e};  
+        System.out.println("Elements of given array present on odd position: ");  
+        //Loop through the array by incrementing value of i by 2  
+        for (int i = 0; i < arr.length; i = i+2) {  
+            System.out.println(arr[i]);  
+        }  
+    }  
+}  
+	}
+}
