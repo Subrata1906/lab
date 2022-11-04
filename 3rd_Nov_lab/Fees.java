083553c4721555b04aefc77da3c9e578b1ce6554
Subrata1906 <110475131+Subrata1906@users.noreply.github.com>
2022-11-04 13:59:26 +0530

    Add files via upload

---
+++ 3rd_Nov_lab/Fees.java
@@ -0,0 +1,15 @@
+//Q1. Problem stetment 3
+public class Fees {
+public void validateFees(Double Fees) {  // method for checking fees is infinite or not
+if(Fees.isInfinite()) { // condition check
+  System.out.println("Fees is infinite");
+  }
+  else{
+  System.out.println("Fees is not infinite");
+   }
+   System.out.println(Fees.byteValue());  //print byte value of fees
+  }
+public static void main(String[] args) { 	// main method 
+Fees ob = new Fees();					//object of the Student class
+  ob.validateFees(750d);
+}}
